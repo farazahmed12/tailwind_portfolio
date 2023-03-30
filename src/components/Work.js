4d0d4faa88b3d88064f6f519c91f96bbf18bd518
Faraz Ahmed <masterdot707@gmail.com>
2023-03-30 16:23:36 +0500

    Created Works.js

---
+++ src/components/Work.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Work = () => {
+  return (
+    <div name="work" className="w-full h-full text-gray-300 bg-[#0a192f]">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="">
+          <p>Works</p>
+          <p>Check here some of my recent works.</p>
+        </div>
+
+        <div>
+          <div>
+            <div>
+              <span></span>
+              <div>
+                <a href="/">
+                  <button></button>
+                </a>
+                <a href="/">
+                  <button></button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
